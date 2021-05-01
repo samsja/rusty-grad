b130e0a292bca40bcb5d4202b3523f0a8b68e734
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-05-01 20:04:21 +0200

    TDD this test should work

--- rusty_grad/tests/autograd.rs
+++ rusty_grad/tests/autograd.rs
@@ -13,3 +13,35 @@ fn test_simple_autograd() {
     assert_eq!(x.borrow().grad, 1.0);
     assert_eq!(y.borrow().grad, 3.0);
 }
+
+#[test]
+fn test_simple_autograd_with_const() {
+    let x_ = VariableRef::new(Variable::new(4.0));
+    let y_ = VariableRef::new(Variable::new(3.0));
+
+    let x = x_.clone();
+    let y = y_.clone();
+
+    let mut z = z = (x + 2) / (y - 4);
+
+    z.backward();
+
+    assert_eq!(x_.borrow().grad, -1.0);
+    assert_eq!(y_.borrow().grad, 6.0);
+}
+
+#[test]
+fn test_complex_autograd() {
+    let x_ = VariableRef::new(Variable::new(4.0));
+    let y_ = VariableRef::new(Variable::new(3.0));
+
+    let x = x_.clone();
+    let y = y_.clone();
+
+    let mut z = (x + x * x + y) / (x * y + x);
+
+    z.backward();
+
+    assert_eq!(x_.borrow().grad, 0.203125);
+    assert_eq!(y_.borrow().grad, -0.296875);
+}
